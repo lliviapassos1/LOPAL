c3f7dd5f3dcff28b60b0667269b05c3fd6a6d166
lliviapassos1 <livia.campos@edu.senai.br>
2026-05-20 09:22:41 -0300

    Add files via upload

--- listaDeExercícios1/lista py/1exercicio.py
+++ listaDeExercícios1/lista py/1exercicio.py
@@ -2,3 +2,4 @@
 nome = input("Qual é seu nome?")
 idade = input("Coloque a sua idade: ")
 email = input("Digite seu email, por favor: ")
+print("Olá", nome,  "!Você tem" ,idade, "idade", "Seu email é " ,email)
